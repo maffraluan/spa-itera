a5e09d2a090585fa4419930b9ee902da8c7042a5
Luan Maffra <luanmaffra@Luans-MacBook-Pro.local>
2021-03-04 02:18:45 -0300

    Added links into external pages

--- src/components/_Templates/Template.WhoIAm.jsx
+++ src/components/_Templates/Template.WhoIAm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import dotted from '../../assets/dotted.png';
 
 import './whoIAm.styles.scss';
@@ -15,7 +16,7 @@ function TemplateWhoIAm() {
       <h1>Who am I<span>?</span></h1>
       <p>Ai você fala o seguinte: "- Mas vocês acabaram isso?" Vou te falar: -"Não, está em andamento!" Tem obras que "vai" durar pra depois de 2010. Agora, por isso, nós já não desenhamos, não começamos a fazer projeto do que nós "podêmo fazê"? 11, 12, 13, 14... Por que é que não?</p>
 
-      <a href="/findoutmore">
+      <a href="http://thiagoh.github.io/sublime-dilmes-ipsum/" rel={'Dilma Ipsum'} target={'_blank'} >
         Find out more
       </a>
     </div>
